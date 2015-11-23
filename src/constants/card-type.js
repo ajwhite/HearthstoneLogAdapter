316c792511b2d9dea1c6e066640695bf390a46b8
Atticus White <contact@atticuswhite.com>
2015-11-23 06:40:50 -0500

    log constants

---
+++ src/constants/card-type.js
@@ -0,0 +1,16 @@
+'use strict';
+const CARD_TYPE = {
+  INVALID: 0,
+  GAME: 1,
+  PLAYER: 2,
+  HERO: 3,
+  MINION: 4,
+  ABILITY: 5,
+  ENHANCEMENT: 6,
+  WEAPON: 7,
+  ITEM: 8,
+  TOKEN: 9,
+  HERO_POWER: 10
+};
+
+export default CARD_TYPE;
---
+++ src/constants/zone.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const ZONE = {
+  INVALID: -1,
+  CREATED: 0,
+  PLAY: 1,
+  DECK: 2,
+  HAND: 3,
+  GRAVEYARD: 4,
+  REMOVEDFROMGAME: 5,
+  SETASIDE: 6,
+  SECRET: 7
+};
+
+export default ZONE;
